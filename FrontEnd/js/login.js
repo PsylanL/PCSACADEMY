57935a93666af5e366a088d23f3632f77f1eae63
Juan David Uribe <juan_uribe82201@elpoli.edu.co>
2022-10-25 22:40:46 -0500

    (BUILD) fix register method and security breach

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -3,32 +3,6 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY>0)
 });
 
-
-async function registerStudent (){
-    let student = {};
-    student.id = document.getElementById("id").value;
-    student.name = document.getElementById("name").value;
-    student.lastName = document.getElementById("lastname").value;
-    student.years = document.getElementById("years").value;
-    student.email = document.getElementById("email").value;
-    student.phone = document.getElementById("phone").value;
-    student.password = document.getElementById("password").value;
-    if(student.password == document.getElementById('password2').value){
-        alert('hola2');
-        const request = await fetch('http://localhost:8080/api/student/register', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(student)
-        });
-        alert('Registrado');
-    } else {
-        alert('porfavor verifique que las contrasenas coincidan');
-    }
-}
-
 async function loginStudent () {
     let student = {};
     student.email = document.getElementById("login-email").value;
