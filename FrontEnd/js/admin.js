e2c744e35bce33cc21c806f3521d93611654a946
DanielSalazar88 <daniel_salazar82201@elpoli.edu.co>
2022-11-17 21:53:46 -0500

    (BUILD/FEAT) the development of HU2 was continued

--- FrontEnd/js/admin.js
+++ FrontEnd/js/admin.js
@@ -31,7 +31,7 @@ async function ListAsignaturesTeachers() {
     let response = await request.json();
 
     this.teachersWithAsignatures = response;
-    console.log(teachersWithAsignatures);
+    //console.log(teachersWithAsignatures);
 
     list(this.teachersWithAsignatures);
 
@@ -160,8 +160,103 @@ function openModalEnroll() {
     })
 
     ModalEnroll.show();
+    listTeachers();
+    listAsignatures();
+    document.getElementById("selected_name_teacher").value = "emty";
+    document.getElementById("selected_asignature").value = "emty";
 }
 
 function closeModalEnroll() {
     ModalEnroll.hide();
+}
+
+
+
+//ENROLL ASIGNATURES TO TEACHERS
+
+var teacherOptions = '';
+var AsignaturesOptions = '';
+
+async function listTeachers() {
+    const getTeachers = "http://localhost:8080/api/teacher/list";
+    let request = await fetch(getTeachers);
+    let response = await request.json();
+    this.teacherOptions = response;
+    console.log(teacherOptions);
+
+    var selectOp = document.getElementById("nameTeacher");
+
+    for(let i = 0; i < teacherOptions.length; i++){
+        selectOp.options[i+1] = new Option(teacherOptions[i].name + " " + 
+        teacherOptions[i].lastName, "value = " + i);
+    }
+
+}
+
+async function listAsignatures() {
+    const getAsignatures = "http://localhost:8080/api/asignature/list";
+    let request = await fetch(getAsignatures);
+    let response = await request.json();
+    this.AsignaturesOptions = response;
+    console.log(AsignaturesOptions);
+
+    var selectOp = document.getElementById("nameAsignature");
+
+    for(let i = 0; i < AsignaturesOptions.length; i++){
+        selectOp.options[i+1] = new Option(AsignaturesOptions[i].name, "value = " + i);
+    }
+
+}
+
+async function selectChange() {
+
+    
+
+    var valSelect = document.getElementById('nameTeacher');
+    var selected = valSelect.options[valSelect.selectedIndex].text;
+    var ban = 0;
+
+    for (let as of teacherOptions) {
+        if (selected == as.name + " " + as.lastName) {
+            console.log(as);
+            document.getElementById("selected_name_teacher").value = as.id;
+            ban = 1;
+        }
+    }
+    if (ban == 0) {
+        document.getElementById("selected_name_teacher").value = "empty";
+
+
+    }
+    
+
+
+}
+
+async function selectChangeAsignature() {
+
+    var valSelect = document.getElementById('nameAsignature');
+    var selected = valSelect.options[valSelect.selectedIndex].text;
+    var ban = 0;
+
+    for (let as of AsignaturesOptions) {
+        if (selected == as.name) {
+            console.log(as);
+            if(as.course == 1){
+                document.getElementById("selected_asignature").value = "Motorcycle course A2";
+            } else if(course == 2){
+                
+            }
+            
+            ban = 1;
+        }
+    }
+    if (ban == 0) {
+        document.getElementById("selected_asignature").value = "empty";
+        
+
+    }
+    
+
+
 }
