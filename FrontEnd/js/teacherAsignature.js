36c7cc67bf8c6d2529619d5075deea6887af8110
Juan David Uribe <juan_uribe82201@elpoli.edu.co>
2022-11-23 20:16:29 -0500

    (FEAT) download list of students as excel

--- FrontEnd/js/teacherAsignature.js
+++ FrontEnd/js/teacherAsignature.js
@@ -1,14 +1,14 @@
 var data = '';
 
-async function classgroup (id) {
+async function classgroup(id) {
     const getClassGroup = "http://localhost:8080/api/teacher/classgroup/" + id;
-    let request = await fetch (getClassGroup);
+    let request = await fetch(getClassGroup);
     let respose = await request.json();
     this.data = respose;
-    
+
     for (let i = 0; i < this.data.length; i++) {
         const getAsignature = "http://localhost:8080/api/classgroup/getasignature/" + this.data[i].id;
-        let request = await fetch (getAsignature);
+        let request = await fetch(getAsignature);
         let respose = await request.text();
         this.data[i].name = respose
     }
@@ -18,58 +18,58 @@ async function classgroup (id) {
 
 window.onload = classgroup(localStorage.id);
 
-function cards (list) {
+function cards(list) {
     list.forEach(element => {
 
         let cardImg = document.createElement('div');
-        cardImg.innerHTML =  '<img src="'+element.img+'" class="card-img-top" alt="...">' ;
+        cardImg.innerHTML = '<img src="' + element.img + '" class="card-img-top" alt="...">';
 
         let cardHeader = document.createElement('div');
-        cardHeader.className ="card-body";
-        cardHeader.id="card-header";
-        cardHeader.innerHTML =  '<h4 class="card-title">'+element.name+'</h4>'+
-                                '<p class="card-text">'+element.description+'</p>';
+        cardHeader.className = "card-body";
+        cardHeader.id = "card-header";
+        cardHeader.innerHTML = '<h4 class="card-title">' + element.name + '</h4>' +
+            '<p class="card-text">' + element.description + '</p>';
 
         let cardList = document.createElement('ul');
-        cardList.className ='list-group list-group-flush';
-        cardList.id="card-list";
-        cardList.innerHTML =    '<li class="list-group-item">' +
-                                    '<h5>Schedule</h5>' +
-                                    '<span>'+element.schedule+'</span>' +
-                                '</li>' +
-                                '<li class="list-group-item">' +
-                                    '<button type="button" class="btn btn-primary btn-card" onclick="functionToList('+element.id+')" aria-expanded="false">Student list</button>' +
-                                '</li>' +
-                                '<li class="list-group-item">' +
-                                    '<button type="button" class="btn btn-primary btn-card" onclick="updateDescription('+element.id+')" aria-expanded="false">modify description</button>' +
-                                '</li>'
+        cardList.className = 'list-group list-group-flush';
+        cardList.id = "card-list";
+        cardList.innerHTML = '<li class="list-group-item">' +
+            '<h5>Schedule</h5>' +
+            '<span>' + element.schedule + '</span>' +
+            '</li>' +
+            '<li class="list-group-item">' +
+            '<button type="button" class="btn btn-primary btn-card" onclick="functionToList(' + element.id + ')" aria-expanded="false">Student list</button>' +
+            '</li>' +
+            '<li class="list-group-item">' +
+            '<button type="button" class="btn btn-primary btn-card" onclick="updateDescription(' + element.id + ')" aria-expanded="false">modify description</button>' +
+            '</li>'
 
         const div = document.createElement('div');
         div.appendChild(cardImg);
         div.appendChild(cardHeader);
         div.appendChild(cardList);
         div.className = 'card';
-        div.style="width: 18rem;";
-        div.id="card-parent";
-        
+        div.style = "width: 18rem;";
+        div.id = "card-parent";
+
         let parent = document.getElementById('contenedor-cards');
         parent.appendChild(div);
     });
-    
+
 }
 
 async function updateDescription(id) {
-    console.log ('voy a modificar la descripcion de:' + id);
-    
+    console.log('voy a modificar la descripcion de:' + id);
+
 }
 
-async function functionToList (id) {
+async function functionToList(id) {
     openModalList();
-    console.log ('voy a listar a:' + id)
+    console.log('voy a listar a:' + id)
     var students = {};
 
     const getStudents = "http://localhost:8080/api/classgroup/getStudents/" + id;
-    let request = await fetch (getStudents);
+    let request = await fetch(getStudents);
     let respose = await request.json();
     students = respose;
     console.log(students);
@@ -77,7 +77,7 @@ async function functionToList (id) {
     let StudentsTable = document.getElementById('tableStudents');
     let tableBody = document.getElementById('tbody');
 
-    for(element of students) {
+    for (element of students) {
         let row = document.createElement('tr');
         row.className = 'table-light'
         let td = document.createElement('td');
@@ -91,7 +91,7 @@ async function functionToList (id) {
         row.appendChild(td);
 
         td = document.createElement('td');
-        td.innerHTML = '<div class="size"><button class="fa-solid fa-paper-plane btn btn-outline-primary btn-sm" onclick="deleteClient(' + element.id +')" id="btnDelete"></button></div>';
+        td.innerHTML = '<div class="size"><button class="fa-solid fa-paper-plane btn btn-outline-primary btn-sm" onclick="deleteClient(' + element.id + ')" id="btnDelete"></button></div>';
         row.appendChild(td);
 
         tableBody.appendChild(row);
@@ -99,17 +99,29 @@ async function functionToList (id) {
     StudentsTable.appendChild(tableBody);
 }
 
-var ModalEnroll = '';
+var modalList = '';
 function openModalList() {
-    ModalEnroll = new bootstrap.Modal(document.getElementById("modalList"), {
+    modalList = new bootstrap.Modal(document.getElementById("modalList"), {
         keyboard: false
     })
 
-    ModalEnroll.show();
+    modalList.show();
 }
 
 function closeModalList() {
     tableBody = document.getElementById('tbody');
     tableBody.innerHTML = '';
-    ModalEnroll.hide();
+    modalList.hide();
 }
+
+var tableToExcel = (function () {
+    var uri = 'data:application/vnd.ms-excel;base64,'
+        , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
+        , base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))) }
+        , format = function (s, c) { return s.replace(/{(\w+)}/g, function (m, p) { return c[p]; }) }
+    return function (table, name) {
+        if (!table.nodeType) table = document.getElementById(table)
+        var ctx = { worksheet: name || 'Worksheet', table: table.innerHTML }
+        window.location.href = uri + base64(format(template, ctx))
+    }
+})()
